c3ec4c68f6771280f0ab0d8694a18a2baf2a4c94
Dan Fellner <dfellner92@gmail.com>
2021-10-01 17:43:20 -0400

    added both

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
+const getRandomDog = async () => {
+  const dog = await fetch("https://dog.ceo/api/breeds/image/random");
+  dogToJSON(dog);
+};
 
+const dogToJSON = async (dog) => {
+  let objDog = await dog.json();
+  console.log(objDog);
+};
 
-
+getRandomDog();
 
 const longestString = (arr) => {
-    let longest = ''
-    let finalArr = []
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].length >= longest.length) {
-        longest = arr[i]
-        finalArr.push(longest)
-        
-      } else {
-        longest = longest
-      }
-    }
-    if (finalArr.length > 1) {
-      return finalArr
+  let longest = "";
+  let finalArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length >= longest.length) {
+      longest = arr[i];
+      finalArr.push(longest);
     } else {
-      return longest
+      longest = longest;
     }
   }
-  
-  
-  console.log(longestString(["apple", "thre", "four"]))
+  if (finalArr.length > 1) {
+    return finalArr;
+  } else {
+    return longest;
+  }
+};
+
+console.log(longestString(["apple", "thre", "four"]));
